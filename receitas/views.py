90e939ded46796d3a705d5b983e523d220ebaffb
arthurcezars <arthurcezars@gmail.com>
2022-05-21 13:55:28 -0300

    Alterado o template para receber os dados dinamicamente do back, criado o modelo de receitas e mapeado para o banco

--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, template_name='index.html')
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de legumes',
+        3: 'Sorvete'
+    }
+
+    dados = {
+        'nome_das_receitas': receitas
+    }
+    return render(request, template_name='index.html', context=dados)
 
 def receita(request):
     return render(request, template_name='receita.html')
